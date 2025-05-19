fe9ba6fd7af27eb6e839cd1ddfb3fefe547a279b
Gouvernon Stan <stan@gouvernon.org>
2025-05-19 22:00:35 +0200

    feat:created all projects.

---
+++ Client/Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Client.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ ECSDatabase/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ECSDatabase;
+
+public class Class1
+{
+}
