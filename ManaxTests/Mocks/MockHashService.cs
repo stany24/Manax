cebeb2cc81a916050f892e43d1a960a3d63a759c
Gouvernon Stan <stan.gouvernon@he-arc.ch>
2025-08-26 13:29:23 +0200

    test: added tests for the user controller

---
+++ ManaxTests/Mocks/MockHashService.cs
@@ -0,0 +1,46 @@
+using ManaxServer.Services.Hash;
+
+namespace ManaxTests.Mocks;
+
+public class MockHashService : IHashService
+{
+    private const string HashedPassword = "hashed";
+    private List<string> HashPasswordCalls { get; } = [];
+    private List<(string password, string storedHash)> VerifyPasswordCalls { get; } = [];
+
+    public string HashPassword(string password)
+    {
+        HashPasswordCalls.Add(password);
+        return password + HashedPassword;
+    }
+
+    public bool VerifyPassword(string password, string storedHash)
+    {
+        VerifyPasswordCalls.Add((password, storedHash));
+        return storedHash == password + HashedPassword;
+    }
+
+    public void VerifyHashPasswordCalled(string expectedPassword)
+    {
+        if (!HashPasswordCalls.Contains(expectedPassword))
+            throw new Exception($"HashPassword was not called with password: {expectedPassword}");
+    }
+
+    public void VerifyHashPasswordNotCalled()
+    {
+        if (HashPasswordCalls.Count != 0)
+            throw new Exception("HashPassword was called when it should not have been");
+    }
+
+    public void VerifyVerifyPasswordCalled(string expectedPassword)
+    {
+        if (VerifyPasswordCalls.All(call => call.password != expectedPassword))
+            throw new Exception($"VerifyPassword was not called with password: {expectedPassword}");
+    }
+
+    public void Reset()
+    {
+        HashPasswordCalls.Clear();
+        VerifyPasswordCalls.Clear();
+    }
+}
---
+++ ManaxTests/Mocks/MockJwtService.cs
@@ -0,0 +1,18 @@
+using ManaxServer.Models.User;
+using ManaxServer.Services.Jwt;
+
+namespace ManaxTests.Mocks;
+
+public class MockJwtService : IJwtService
+{
+    public string GetSecretKey()
+    {
+        return "test-secret-key";
+    }
+
+    public string GenerateToken(User user)
+    {
+        return $"{user.Id}-jwt-token";
+    }
+}
+
