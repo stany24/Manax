ed2ee32934c61588284f01bb558690d089e86b54
Gouvernon Stan <stan.gouvernon@he-arc.ch>
2025-09-15 16:20:44 +0200

    feat(client): remember login information

---
+++ ManaxClient/Models/LoginValues.cs
@@ -0,0 +1,8 @@
+namespace ManaxClient.Models;
+
+public class LoginValues
+{
+    public string Username { get; set; }
+    public string Host { get; set; }
+    public int? Port { get; set; }
+}
--- ManaxClient/ViewModels/Pages/Login/LoginPageViewModel.cs
+++ ManaxClient/ViewModels/Pages/Login/LoginPageViewModel.cs
@@ -1,7 +1,10 @@
 using System;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
+using ManaxClient.Models;
 using ManaxClient.ViewModels.Pages.Home;
 using ManaxLibrary;
 using ManaxLibrary.ApiCaller;
@@ -14,17 +17,20 @@ public partial class LoginPageViewModel : PageViewModel
 {
     [ObservableProperty] private bool _canLogin = true;
     [ObservableProperty] private string _emoji = "🔑";
-    [ObservableProperty] private string _host = "http://127.0.0.1";
+    [ObservableProperty] private string _loginError = string.Empty;
     private bool _isAdmin;
     private bool _isOwner;
-    [ObservableProperty] private string _loginError = string.Empty;
+    private readonly string _saveFile;
+    [ObservableProperty] private string _host = string.Empty;
     [ObservableProperty] private string _password = string.Empty;
-    [ObservableProperty] private int? _port = 5246;
+    [ObservableProperty] private int? _port;
     [ObservableProperty] private string _username = string.Empty;
 
     public LoginPageViewModel()
     {
+        _saveFile = Path.Combine(Directory.GetCurrentDirectory(), "login.json");
         ControlBarVisible = false;
+        TryLoadSavedLogin();
     }
 
     public void Login()
@@ -60,11 +66,56 @@ public partial class LoginPageViewModel : PageViewModel
             _isOwner = self.Role is UserRole.Owner;
             InfoEmitted?.Invoke(this, $"Logged in as {self.Username} ({self.Role})");
             Logger.LogInfo($"Logged in as {self.Username} ({self.Role})");
+            SaveLoginValues();
             PageChangedRequested?.Invoke(this, new HomePageViewModel());
         }
         catch (Exception)
         {
             Dispatcher.UIThread.Post(() => { LoginError = "Unknown error"; });
+        }
+    }
+
+    private void SaveLoginValues()
+    {
+        LoginValues loginValues = new()
+        {
+            Host = Host,
+            Port = Port,
+            Username = Username
+        };
+        try
+        {
+            if(File.Exists(_saveFile)){ File.Delete(_saveFile);}
+            using FileStream fileStream = File.OpenWrite(_saveFile);
+            using StreamWriter writer = new(fileStream);
+            string serialize = JsonSerializer.Serialize(loginValues);
+            writer.Write(serialize);
+            writer.Flush();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+    
+    private void TryLoadSavedLogin()
+    {
+        if (!File.Exists(_saveFile)) return;
+        try
+        {
+            using FileStream fileStream = File.OpenRead(_saveFile);
+            using StreamReader reader = new(fileStream);
+            string content = reader.ReadToEnd();
+            LoginValues? loginValues = JsonSerializer.Deserialize<LoginValues>(content);
+            if (loginValues == null) return;
+            Host = loginValues.Host;
+            Port = loginValues.Port;
+            Username = loginValues.Username;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
         }
     }
 
