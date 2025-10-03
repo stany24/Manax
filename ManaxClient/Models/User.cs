b893469a956eb57e7cbba0d9e60f40a3c9492bbd
Gouvernon Stan <stan@gouvernon.org>
2025-10-03 12:36:43 +0200

    refactor(client): improved binding for users

--- ManaxClient/Models/User.cs
+++ ManaxClient/Models/User.cs
@@ -1,5 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
+using DynamicData;
+using ManaxLibrary;
+using ManaxLibrary.ApiCaller;
 using ManaxLibrary.DTO.User;
 using ManaxLibrary.Notifications;
 
@@ -7,11 +14,21 @@ namespace ManaxClient.Models;
 
 public partial class User:ObservableObject
 {
+    public static readonly SourceCache<User, long> Users = new (x => x.Id);
+    private static readonly object UsersLock = new();
+    
     [ObservableProperty] private long _id;
     [ObservableProperty] private string _username = string.Empty;
     [ObservableProperty] private UserRole _role;
     [ObservableProperty] private DateTime _lastLogin;
 
+    static User()
+    {
+        ServerNotification.OnUserCreated += OnUserCreated;
+        ServerNotification.OnUserDeleted += OnUserDeleted;
+        LoadUsers();
+    }
+    
     public User(UserDto dto)
     {
         FromUserDto(dto);
@@ -21,6 +38,50 @@ public partial class User:ObservableObject
     ~User()
     {
         ServerNotification.OnUserUpdated -= OnUserUpdated;
+    }
+
+    private static void LoadUsers()
+    {
+        Task.Run(async () =>
+        {
+            Optional<List<long>> usersIdsResponse = await ManaxApiUserClient.GetUsersIdsAsync();
+            if (usersIdsResponse.Failed)
+            {
+                return;
+            }
+
+            List<long> ids = usersIdsResponse.GetValue();
+            foreach (long id in ids)
+            {
+                Optional<UserDto> userResponse = await ManaxApiUserClient.GetUserAsync(id);
+                if (userResponse.Failed)
+                {
+                    continue;
+                }
+
+                UserDto dto = userResponse.GetValue();
+                lock (UsersLock)
+                {
+                    Users.AddOrUpdate(new User(dto));
+                }
+            }
+        });
+    }
+    
+    private static void OnUserDeleted(long id)
+    {
+        lock (UsersLock)
+        {
+            Users.RemoveKey(id);   
+        }
+    }
+
+    private static void OnUserCreated(UserDto user)
+    {
+        lock (UsersLock)
+        {
+            Users.AddOrUpdate(new User(user));
+        }
     }
 
     private void OnUserUpdated(UserDto dto)
--- ManaxClient/ViewModels/Pages/User/UsersPageViewModel.cs
+++ ManaxClient/ViewModels/Pages/User/UsersPageViewModel.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
+using DynamicData;
+using DynamicData.Binding;
 using ManaxClient.ViewModels.Popup.ConfirmCancel;
 using ManaxClient.ViewModels.Popup.ConfirmCancel.Content;
 using ManaxLibrary;
@@ -15,54 +17,18 @@ using ManaxLibrary.Notifications;
 
 namespace ManaxClient.ViewModels.Pages.User;
 
-public partial class UsersPageViewModel : PageViewModel
+public class UsersPageViewModel : PageViewModel
 {
-    [ObservableProperty] private ObservableCollection<Models.User> _users = [];
+    private readonly ReadOnlyObservableCollection<Models.User> _users;
+    public ReadOnlyObservableCollection<Models.User> Users => _users;
 
     public UsersPageViewModel()
     {
-        Task.Run(async () =>
-        {
-            Optional<List<long>> usersIdsResponse = await ManaxApiUserClient.GetUsersIdsAsync();
-            if (usersIdsResponse.Failed)
-            {
-                InfoEmitted?.Invoke(this, usersIdsResponse.Error);
-                return;
-            }
-
-            List<long> ids = usersIdsResponse.GetValue();
-            foreach (long id in ids)
-            {
-                Optional<UserDto> userResponse = await ManaxApiUserClient.GetUserAsync(id);
-                if (userResponse.Failed)
-                {
-                    InfoEmitted?.Invoke(this, userResponse.Error);
-                    continue;
-                }
-
-                Dispatcher.UIThread.Post(() => Users.Add(new Models.User(userResponse.GetValue())));
-            }
-        });
-        ServerNotification.OnUserCreated += OnUserCreated;
-        ServerNotification.OnUserDeleted += OnUserDeleted;
-    }
-
-    ~UsersPageViewModel()
-    {
-        ServerNotification.OnUserCreated -= OnUserCreated;
-        ServerNotification.OnUserDeleted -= OnUserDeleted;
-    }
-
-    private void OnUserDeleted(long userId)
-    {
-        Models.User? user = Users.FirstOrDefault(u => u.Id == userId);
-        if (user == null) return;
-        Dispatcher.UIThread.Post(() => Users.Remove(user));
-    }
-
-    private void OnUserCreated(UserDto user)
-    {
-        Dispatcher.UIThread.Post(() => Users.Add(new Models.User(user)));
+        SortExpressionComparer<Models.User> comparer = SortExpressionComparer<Models.User>.Descending(user => user.Username);
+        Models.User.Users
+            .Connect()
+            .SortAndBind(out _users, comparer)
+            .Subscribe();
     }
 
     public void DeleteUser(Models.User user)
