0ad1bc63d65ce0b2b98beda78b65149858f459a0
Gouvernon Stan <stan@gouvernon.org>
2025-10-08 13:39:30 +0200

    feat(client): added localization files

---
+++ ManaxClient/Assets/Localization/ResXLocalizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using Jeek.Avalonia.Localization;
+
+namespace ManaxClient.Assets.Localization;
+
+public class ResXLocalizer : BaseLocalizer
+{
+    public override void Reload()
+    {
+        if (_languages.Count == 0)
+        {
+            _languages.Add("en");
+            _languages.Add("fr");
+        }
+
+        ValidateLanguage();
+
+        Resources.Culture = new CultureInfo(_language);
+
+        _hasLoaded = true;
+
+        UpdateDisplayLanguages();
+    }
+
+    protected override void OnLanguageChanged()
+    {
+        Reload();
+    }
+
+    public override string Get(string key)
+    {
+        if (!_hasLoaded)
+            Reload();
+
+        var langString = Resources.ResourceManager.GetString(key, Resources.Culture);
+        return langString != null
+            ? langString.Replace("\\n", "\n")
+            : $"{Language}:{key}";
+    }
+}
