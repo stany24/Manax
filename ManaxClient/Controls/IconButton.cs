3aedb5ef9bdaad687a7b63dfd16e0c1acad6c30e
Gouvernon Stan <stan@gouvernon.org>
2026-02-08 18:18:03 +0100

    feat(client): updated UI for the sidebar

--- ManaxClient/Controls/IconButton.cs
+++ ManaxClient/Controls/IconButton.cs
@@ -2,7 +2,7 @@ using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Layout;
-using Avalonia.Markup.Xaml.MarkupExtensions;
+using Avalonia.Media;
 using Material.Icons;
 using Material.Icons.Avalonia;
 
@@ -12,6 +12,9 @@ public class IconButton : Button
 {
     public static readonly StyledProperty<MaterialIconKind?> IconProperty =
         AvaloniaProperty.Register<IconButton, MaterialIconKind?>(nameof(Icon), MaterialIconKind.Error);
+    
+    public static readonly StyledProperty<IBrush?> IconColorProperty =
+        AvaloniaProperty.Register<IconButton, IBrush?>(nameof(Icon));
 
     public static readonly StyledProperty<string?> TextProperty =
         AvaloniaProperty.Register<IconButton, string?>(nameof(Text));
@@ -23,7 +26,8 @@ public class IconButton : Button
             Width = 20,
             Height = 20,
             Margin = new Thickness(0, 0, 5, 0),
-            [!MaterialIcon.KindProperty] = this[!IconProperty]
+            [!MaterialIcon.KindProperty] = this[!IconProperty],
+            [!ForegroundProperty] = this[!IconColorProperty]
         };
         
         TextBlock textBlock = new()
@@ -49,8 +53,12 @@ public class IconButton : Button
         
         Content = stackPanel;
         RenderTransformOrigin = new RelativePoint(0.5, 0.5, RelativeUnit.Relative);
-        this[!BackgroundProperty] = new DynamicResourceExtension("MaterialCardBackgroundBrush");
-        this[!BorderBrushProperty] = new DynamicResourceExtension("MaterialPrimaryLightBrush");
+    }
+    
+    public IBrush? IconColor
+    {
+        get => GetValue(IconColorProperty);
+        set => SetValue(IconColorProperty, value);
     }
     
     public MaterialIconKind? Icon
